064d4b6e4e31ce92650442888171b5a28879a083
Borys <jozwiak.borys@gmail.com>
2026-02-28 16:05:02 +0100

    Create quadratic.cpp

---
+++ quadratic.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  // Declaration of a, b, c
+  double a, b, c;
+  
+  // Asking user to provide numbers for a, b, c
+  std::cout << "Enter a: ";
+  std::cin >> a;
+
+  std::cout << "Enter b: ";
+  std::cin >> b;
+  
+  std::cout << "Enter c: ";
+  std::cin >> c;
+
+  // Declaration of roots
+  double root1, root2;
+  
+  // Solving root1 and root2
+  root1 = (-b + std::sqrt(b*b - 4*a*c)) / (2*a);  
+  root2 = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
+
+  std::cout << "Root 1 is " << root1 << "\n";
+  std::cout << "Root 2 is " << root2 << "\n";
+}
