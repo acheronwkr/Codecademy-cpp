e7ec5b9c7c651712b73b238a4385a27908c5b5da
Borys <jozwiak.borys@gmail.com>
2026-03-02 16:50:48 +0100

    Add files via upload

---
+++ UFO/ufo_func.hpp
@@ -0,0 +1,7 @@
+#include <vector>
+
+// Declare functions
+void greet();
+void end_game(std::string answer, std::string codeword);
+void display_misses(int misses);
+void display_status(std::vector<char> incorrect, std::string answer);
