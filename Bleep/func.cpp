cf6f2b9725e136d6b5bdbb9563f7b329bcfb36fa
Borys <jozwiak.borys@gmail.com>
2026-03-13 14:47:45 +0100

    Create func.cpp

---
+++ Bleep/func.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include "func.hpp"
+
+void bleep(std::string word, std::string &text) {
+  
+  int match = text.find(word);
+
+  while (match != -1) {
+    for (int i = 0; i < word.length(); i++) {
+      text[match + i] = '*';
+    }
+    match = text.find(word, match + word.length());
+  }
+}
