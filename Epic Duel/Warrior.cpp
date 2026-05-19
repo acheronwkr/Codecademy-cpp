44c0ec98fe23e4272165f13717ab4afe07214923
Borys <jozwiak.borys@gmail.com>
2026-05-19 12:01:48 +0200

    Implement Warrior class with attack methods

--- Epic Duel/Warrior.cpp
+++ Epic Duel/Warrior.cpp
@@ -1,1 +1,11 @@
 #include "Warrior.hpp"
+
+Warrior::Warrior(std::string name, int health, int power) : Character(name, health, power){}
+
+void Warrior::attack() {
+  std::cout << "Swings a might sword!" << std::endl;
+}
+
+void Warrior::specialAttack() {
+  std::cout << name << " a devastating double stike!" << std::endl;
+}
