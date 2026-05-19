7f39ab755b8ad3d34a31a1fee761abe0a219b309
Borys <jozwiak.borys@gmail.com>
2026-05-19 12:02:46 +0200

    Implement Sorcerer class with attack method

--- Epic Duel/Sorcerer.cpp
+++ Epic Duel/Sorcerer.cpp
@@ -1,1 +1,7 @@
 #include "Sorcerer.hpp"
+
+Sorcerer::Sorcerer(std::string name, int health, int power) : Character(name, health, power){}
+
+void Sorcerer::attack() {
+  std::cout << "Cast a spell!" << std::endl;
+}
