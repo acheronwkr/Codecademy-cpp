a97c42c6f243b5273b80fac38fe0a7c36b8df260
Borys <jozwiak.borys@gmail.com>
2026-05-19 12:03:25 +0200

    Implement character management and attacks
    Added character creation and attack functionality.

--- Epic Duel/main.cpp
+++ Epic Duel/main.cpp
@@ -6,6 +6,26 @@ using namespace std;
 
 int main() {
 
+  vector<Character*> characters;
+
+  characters.push_back(new Warrior("Boras", 200, 100));
+  characters.push_back(new Sorcerer("Bobrys", 100, 150));
+
+  for (Character* character : characters) {
+    character->attack();
+    character->displayInfo();
+  }
+
+  Character* characterPtr = new Warrior("Bober", 80, 200);
+  Warrior* warriorPtr = dynamic_cast<Warrior*>(characters[0]);
+  if (warriorPtr) {
+    warriorPtr->specialAttack();
+  }
+
+  for (int i = 0; i < characters.size(); i++) {
+    delete characters[i];
+  }
+  characters.clear();
 
   return 0;
 }
