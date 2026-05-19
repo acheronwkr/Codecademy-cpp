9791c232b4183bfb0e3245380842563eb6090b66
Borys <jozwiak.borys@gmail.com>
2026-05-19 12:00:48 +0200

    Add Character class with constructors and methods

--- Epic Duel/Character.cpp
+++ Epic Duel/Character.cpp
@@ -1,2 +1,21 @@
 #include "Character.hpp"
 
+Character::Character() {
+  name = "Elden King";
+  health = 100;
+  power = 10;
+}
+
+Character::Character(std::string name, int health, int power){
+  this->name = name;
+  this->health = health;
+  this->power = power;
+}
+
+void Character::attack(){
+  std::cout << "Basic Low Attack" << std::endl;
+}
+
+void Character::displayInfo(){
+  std::cout << "Name: " << name << ", Health: " << health << ", Power: " << power << std::endl;
+}
