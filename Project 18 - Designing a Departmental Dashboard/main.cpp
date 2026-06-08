acaa8403a22b6d42459a57e7a406dc3d82ccf383
Borys <jozwiak.borys@gmail.com>
2026-06-08 10:36:16 +0200

    Creation of marketing and sales teams, added members and printing details

---
+++ Project 18 - Designing a Departmental Dashboard/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Marketing.hpp"
+#include "Sales.hpp"
+
+namespace audit = Marketing::Support;
+
+int main() {
+
+  std::vector<std::string> marketingMembers;
+  std::vector<std::string> salesMembers;
+  
+  Marketing::Team marketingTeam("Stellar Corp", marketingMembers, 500000);
+  marketingTeam.addMember("Scott");
+  marketingTeam.addMember("Nicole");
+  marketingTeam.addMember("Artem");
+
+  Sales::Team salesTeam("Stellar Corp", salesMembers, "London");
+  salesTeam.addMember("Patrik");
+  salesTeam.addMember("Emily");
+  salesTeam.addMember("Clive");
+  salesTeam.printTeamDetails();
+  salesTeam.setLocation("New York");
+  salesTeam.printTeamDetails();
+
+  audit::auditTeam(marketingTeam);
+
+  return 0;
+}
