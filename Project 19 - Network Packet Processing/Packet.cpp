d750f54d80207151c1cc69e715e5ca42f4cfd35d
Borys <jozwiak.borys@gmail.com>
2026-06-19 14:42:24 +0200

    Implement Packet class with type and status handling

---
+++ Project 19 - Network Packet Processing/Packet.cpp
@@ -0,0 +1,106 @@
+#include "Packet.hpp"
+#include <iostream>
+
+using namespace std;
+
+string Packet::getPacketTypeName() const {
+  switch (type) {
+    case PacketType::UNKNOWN:
+      return "UNKNOWN";
+      break;
+    case PacketType::DATA:
+      return "DATA";
+      break;
+    case PacketType::CONTROL:
+      return "CONTROL";
+      break;
+    case PacketType::ERROR:
+      return "ERROR";
+      break;
+    default:
+      return "UNKNOWN ERROR";
+  }
+}
+
+string Packet::getStatusName() const {
+  switch (packetStatus) {
+    case Status::PENDING:
+      return "PENDING";
+      break;
+    case Status::VALID:
+      return "VALID";
+      break;
+    case Status::INVALID:
+      return "INVALID";
+      break;
+    case Status::TIMEOUT:
+      return "TIMEOUT";
+      break;
+    default:
+      return "Unknown Packet Status";
+  }
+}
+
+string Packet::getProtocolName() {
+  switch (protocol) {
+    case Protocol::TCP:
+      return "TCP";
+      break;
+    case Protocol::UDP:
+      return "UDP";
+      break;
+    case Protocol::HTTP:
+      return "HTTP";
+      break;
+    case Protocol::FTP:
+      return "FTP";
+      break;
+    default:
+      return "Unknown Protocol";
+  }
+}
+
+void Packet::validatePacket() {
+  switch (type) {
+    case PacketType::DATA:
+      if (protocol == Protocol::TCP || protocol == Protocol::UDP){
+        packetStatus = Status::VALID;
+      } else {
+        packetStatus = Status::INVALID;
+      }
+      break;
+    case PacketType::CONTROL:
+      if (protocol == Protocol::TCP) {
+        packetStatus = Status::VALID;
+      } else {
+        packetStatus = Status::INVALID;
+      }
+      break;
+    case PacketType::ERROR:
+      packetStatus = Status::VALID;
+      break;
+    case PacketType::UNKNOWN:
+      packetStatus = Status::INVALID;
+      break;
+    default:
+      packetStatus = Status::INVALID;
+  }
+}
+
+bool Packet::isError() {
+  if (packetStatus == Status::INVALID) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void Packet::displayInfo() {
+  cout << "Packet Type: " << getPacketTypeName() << endl;
+  cout << "Protocol: " << getProtocolName() << endl;
+  cout << "Status: "<< getStatusName() << endl;
+}
+
+void Packet::setType(PacketType newType) {
+  type = newType;
+}
