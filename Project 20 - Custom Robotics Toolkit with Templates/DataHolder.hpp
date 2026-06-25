c4d2bfad3ab66b487f2d06842b5690e70b05c8e9
Borys <jozwiak.borys@gmail.com>
2026-06-25 14:06:23 +0200

    Create a header file for DataHolder template class and specialized version of it

---
+++ Project 20 - Custom Robotics Toolkit with Templates/DataHolder.hpp
@@ -0,0 +1,55 @@
+template <typename T>
+class DataHolder {
+  private:
+    T storedValue;
+    T maxValue;
+    T minValue;
+  public:
+    DataHolder(T val, T min, T max) : minValue(min), maxValue(max) {
+      if (val >= min && val <= max) {
+        storedValue = val;
+      } 
+      else if (val > max) {
+        storedValue = max;
+        std::cout << "WARNING: " << val << " is too high! Setting value to " << max << "!" << std::endl;
+      }
+      else if (val < min) {
+        storedValue = min;
+        std::cout << "WARNING: " << val << " is too low! Setting value to " << min << "!" << std::endl;
+      }
+    }
+
+    void setValue(T newValue) {
+      if (newValue >= minValue && newValue <= maxValue) {
+        storedValue = newValue;
+      }
+      else if (newValue > maxValue) {
+        storedValue = maxValue;
+        std::cout << "WARNING: " << newValue << " is too high! Setting value to " << maxValue << "!" << std::endl;
+      }
+      else if (newValue > minValue) {
+        storedValue = minValue;
+        std::cout << "WARNING: " << newValue << " is too low! Setting value to " << minValue << "!" << std::endl;
+      }
+    }
+
+    T getValue() {
+      return storedValue;
+    }
+};
+
+template <>
+class DataHolder<std::string> {
+private:
+    std::string storedValue;
+  public:
+    DataHolder(std::string val) : storedValue(val) {}
+
+    void setValue(std::string newString) {
+      storedValue = newString;
+    }
+
+    std::string getValue() {
+      return storedValue;
+    }
+};
